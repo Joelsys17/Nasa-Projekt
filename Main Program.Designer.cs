53b4362641f2f5ea641ae52654d1db4bb49927c1
Joel Persson <joelpersson@outlook.com>
2017-02-10 12:54:35 +0100

    Bigger screen.
    Bigger screen.

--- Main Program.Designer.cs
+++ Main Program.Designer.cs
@@ -273,7 +273,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
-            this.ClientSize = new System.Drawing.Size(1101, 680);
+            this.ClientSize = new System.Drawing.Size(3013, 680);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
